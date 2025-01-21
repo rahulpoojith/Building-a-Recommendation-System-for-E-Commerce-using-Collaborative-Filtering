fbb7ee700540601b34d5a156210ec6683563f259
rahulpoojith <poojith.p.rahul@gmail.com>
2025-01-21 12:35:23 +0530

    Main page

--- app.py
+++ app.py
@@ -9,5 +9,9 @@ def index():
     return render_template('index.html')
 
 
+@app.route("/main")
+def main():
+    return render_template('main.html')
+
 if __name__=='__main__':
     app.run(debug=True)
